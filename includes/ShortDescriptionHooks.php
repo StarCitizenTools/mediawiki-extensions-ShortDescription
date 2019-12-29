008918b7a1b339ff0b52b3978d9b07069bb20f08
alistair3149 <alistair31494322@gmail.com>
2019-12-29 18:07:20 -0500

    Supress PHP error

--- includes/ShortDescriptionHooks.php
+++ includes/ShortDescriptionHooks.php
@@ -2,24 +2,22 @@
 
 class ShortDescriptionHooks {
 
-	// Register any render callbacks with the parser
+	/* Register any render callbacks with the parser */
 	public static function onParserFirstCallInit( Parser $parser ) {
-
 		$parser->setFunctionHook(
 			'shortdesc',
 			[ self::class, 'handle' ],
 			Parser::SFH_NO_HASH
 		);
 
-		// Create a function hook associating the "getshortdesc" magic word with rendershortdesc()
+		/* Create a function hook associating the "getshortdesc" magic word with rendershortdesc() */
 		$parser->setFunctionHook( 'getshortdesc', [ self::class, 'rendershortdesc' ], Parser::SFH_NO_HASH );
 
 		return true;
 	}
 
-	// Render the output of {{GETSHORTDESC}}.
+	/* Render the output of {{GETSHORTDESC}}. */
 	public static function rendershortdesc( Parser $parser ) {
-
 		$output = '';
 
 		// Check if shortdesc exists, render if exist
@@ -36,15 +34,14 @@ class ShortDescriptionHooks {
      * Extracted from WikiBase
      * See T184000 for related info
 	 */
-   
    /**
-	 * Parser function callback
-	 *
-	 * @param Parser $parser
-	 * @param string $shortDesc Short description of the current page, as plain text.
-	 *
-	 * @return string
-	 */
+	* Parser function callback
+	*
+	* @param Parser $parser
+	* @param string $shortDesc Short description of the current page, as plain text.
+	*
+	* @return string
+	*/
 	public static function handle( Parser $parser, $shortDesc ) {
 		$handler = self::newFromGlobalState();
 		$handler->doHandle( $parser, $shortDesc );
